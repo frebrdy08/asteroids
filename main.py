65df0969f57433229f061d06699191c240e1ce14
frebrdy08 <daniel.lewis.miller@gmail.com>
2025-02-17 17:30:10 -0700

    Completed asteroid field and ship with collision detection

--- main.py
+++ main.py
@@ -1,26 +1,49 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
-
-from constants import (
-    SCREEN_WIDTH,
-    SCREEN_HEIGHT,
-    ASTEROID_KINDS,
-    ASTEROID_MAX_RADIUS,
-    ASTEROID_MIN_RADIUS,
-    ASTEROID_SPAWN_RATE,
-    BACKGROUND_COLOR
-)
+from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+import sys
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    display_clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+
+    player_shape = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+
+    dt = 0
 
     while True:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        updatable.update(dt)
+
+        for obj in asteroids:
+            if obj.collision_detect(player_shape) == True:
+                print("Game Over!")
+                sys.exit()
+
         screen.fill(BACKGROUND_COLOR)
+
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
+
+        dt = display_clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
